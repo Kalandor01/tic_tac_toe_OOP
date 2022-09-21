d609ed98deea53c3b9c03d2aa561865bf0eb6504
Kalandor01 <54396021+Kalandor01@users.noreply.github.com>
2022-09-21 22:20:26 +0200

    fUnNy fix???????

--- field.js
+++ field.js
@@ -1,5 +1,5 @@
-import Cell from "./cell.js"
-import Info from "./info.js"
+import Cell from "../tic_tac_toe_OOP/cell.js"
+import Info from "../tic_tac_toe_OOP/info.js"
 
 class Field {
     #fieldElem
